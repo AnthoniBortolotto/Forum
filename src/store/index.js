64fdd545d293317de8cde92b23fc41adb366ed7f
anthoni Bortolotto Martinelli <antho@outlook.com.br>
2021-04-17 15:01:29 -0300

    Criação de regrasMsg

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,13 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-     Postagems: []
+     Postagems: [],
+     regrasMsg: [
+       v => v == '' || 'A mensagem é obrigatória',
+       v => /[^a-z0-9 A-Zãâõêç.,]+/.test(v) || 'Não é permitido caracteres especiais',
+       v => v.length <= 3 || 'A mensagem deve ter mais de 3 caracteres',
+       v => v.length >= 200 || 'A mensagem deve ter menos de 200 caracteres'
+     ]
   },
   mutations: {
     addPostagem (state, payload){
