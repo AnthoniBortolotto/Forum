10b796e8353188f25166688e6c2ca6d661e2052c
anthoni Bortolotto Martinelli <antho@outlook.com.br>
2021-04-17 14:15:09 -0300

    Criação do cabeçario e das abas

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,74 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-card>
+      <v-toolbar color="primary" dark flat>
+        <v-toolbar-title class="justify-space-between text-center"
+          >Forum</v-toolbar-title
+        >
+        <template v-slot:extension>
+          <v-tabs v-model="abaAtual" right fixed-tabs>
+            <v-tabs-slider color="yellow"></v-tabs-slider>
+            <v-tab v-for="(aba, index) in abas" @click="atualizarAba(aba.rota)" :key="index">
+              {{ aba.nome }}
+            </v-tab>
+          </v-tabs>
+        </template>
+      </v-toolbar>
+
+      <v-tabs-items v-model="abaAtual">
+        <v-tab-item :key="0">
+          <Home />
+        </v-tab-item>
+        <v-tab-item :key="1">
+          <Adicionar />
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
+    <v-main> </v-main>
+    <v-footer color="cyan" class="d-flex justify-center mb-6">
+      {{ new Date().getFullYear() }} | Forum
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Vue from "vue";
+import Home from "./pages/Home.vue";
+import Adicionar from "./pages/Adicionar.vue";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default Vue.extend({
+  name: "App",
+  components: {
+    Home,
+    Adicionar,
+  },
+  data: function () {
+    return {
+      abaAtual: 0,
+      abas: [
+        {
+          nome: "Home",
+          rota: "/",
+        },
+        {
+          nome: "Adicionar",
+          rota: "/adicionar",
+        },
+      ],
+    };
+  },
+  mounted() {
+    if(window.location.href === 'http://localhost:8080/adicionar'){
+        this.abaAtual = 1
+    }
+    else{
+      this.abaAtual = 0
+    }
+  },
+  methods: {
+    atualizarAba: function (rota) {
+        this.$router.push({ path: rota})
+      }
+    }
+  });
+</script>
---
+++ src/pages/Adicionar.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>Adicionar</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
